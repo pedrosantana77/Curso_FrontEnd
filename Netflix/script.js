63c6e7a6679aca770bdd15246d77cffc0f353365
Pedro Henrique <pedrohenriquematiassantana@gmail.com>
2023-09-06 11:41:00 -0300

    Atualizando repositorio

---
+++ Netflix/script.js
@@ -0,0 +1,6 @@
+const login = document.getElementById('login')
+const senha = document.getElementById('senha')
+const form = document.getElementById('formulario')
+const enviar = document.getElementById('entrar')
+
+enviar.addEventListener('click')
